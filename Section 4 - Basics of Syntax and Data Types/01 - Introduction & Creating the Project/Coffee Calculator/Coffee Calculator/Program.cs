5afff1ff3a41e7f3d20c6200dd4b4853c3bc995d
Sergey Boiko <Sergey.Boyko@dataart.com>
2025-05-21 11:42:11 +0200

    Initializes Coffee Calculator project
    Sets up the basic project structure for the Coffee Calculator application.
    This includes creating the solution file, project file, and a basic Program.cs file.

--- Section 4 - Basics of Syntax and Data Types/01 - Introduction & Creating the Project/Coffee Calculator/Coffee Calculator/Program.cs
+++ Section 4 - Basics of Syntax and Data Types/01 - Introduction & Creating the Project/Coffee Calculator/Coffee Calculator/Program.cs
@@ -1,3 +1,1 @@
-﻿int numberOfCoffees = 3; // This variable stores how many coffees the user wants
-double pricePerCoffee = 2.5;
-const double salesTax = 0.07;
+﻿
---
+++ Section 4 - Basics of Syntax and Data Types/02 - Variables Constants and Comments/Coffee Calculator/Coffee Calculator/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberOfCoffees = 3; // This variable stores how many coffees the user wants
+double pricePerCoffee = 2.5;
+const double salesTax = 0.07;
