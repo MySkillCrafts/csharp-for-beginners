6c272a1cf408207b887da00de8f598263af5f810
Sergey Boiko <Sergey.Boyko@dataart.com>
2025-05-26 01:03:08 +0200

    Adds float and double precision demo
    Creates a new console application that demonstrates the difference in precision between float and double data types.
    
    This showcases how double provides a more accurate representation of fractional numbers compared to float.

---
+++ Section 4 - Basics of Syntax and Data Types/05 - Data Types (float and double)/FloatDoubleDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿float floatExample = 1f / 3f;
+double doubleExample = 1d / 3d;
+
+Console.WriteLine("Float vs Double Precision Demo");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine($"Float precision (1/3): {floatExample}");
+Console.WriteLine($"Double precision (1/3): {doubleExample}");
