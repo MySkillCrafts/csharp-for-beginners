e44bfadd5a148ef57c858ccd58b798ba19522d26
Sergey Boiko <Sergey.Boyko@dataart.com>
2025-05-27 11:03:49 +0200

    Adds input validation to coffee calculator
    Improves user experience by handling invalid input.
    
    Instead of crashing the application when a user
    enters a non-numerical value for the number of
    coffees, the program now defaults to one coffee.

--- Section 4 - Basics of Syntax & Data Types/06 - Data Types & Decision Making (bool char if)/CoffeeCalculator/Program.cs
+++ Section 4 - Basics of Syntax & Data Types/06 - Data Types & Decision Making (bool char if)/CoffeeCalculator/Program.cs
@@ -3,31 +3,13 @@ double pricePerCoffee = 2.5;
 const double salesTax = 0.07;
 
 Console.WriteLine("Welcome to our Coffee Calculator!");
-
 Console.Write("How many cups of coffee would you like? ");
-string userInput = Console.ReadLine();
-
-bool isValidNumber = int.TryParse(userInput, out numberOfCoffees);
-
-if (!isValidNumber)
-{
-    Console.WriteLine("Oops! That doesn't look like a valid number. We'll set your order to 1 coffee by default.");
-    numberOfCoffees = 1;
-}
+numberOfCoffees = int.Parse(Console.ReadLine());
 
 Console.Write("Would you like sugar in your coffee? (y/n): ");
 char sugarResponse = char.Parse(Console.ReadLine());
 
-bool wantsSugar;
-
-if (sugarResponse == 'y')
-{
-    wantsSugar = true;
-}
-else
-{
-    wantsSugar = false;
-}
+bool wantsSugar = sugarResponse == 'y';
 
 Console.WriteLine($"Sugar added: {wantsSugar}");
 
