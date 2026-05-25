8f957fbcac5329cd4014b9734557efa9071666f8
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-05-25 15:52:12 +0200

    save progress

---
+++ Section 06 - Methods - Organizing Your Code/02 - Write and Call Your First Method/UnitConverter/UnitConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿
+ShowWelcome();
+
+bool running = true;
+
+while (running)
+{
+    ShowMenu();
+    Console.Write("Your choice: ");
+    string choice = Console.ReadLine();
+    ShowDivider();
+
+    if (choice == "0")
+    {
+        running = false;
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+void ShowWelcome() {
+    Console.WriteLine("==============================");
+    Console.WriteLine("      UNIT CONVERTER");
+    Console.WriteLine("==============================");
+    Console.WriteLine();
+}
+
+void ShowMenu() {
+    Console.WriteLine("What would you like to convert?");
+    Console.WriteLine();
+    Console.WriteLine("1. Fahrenheit >> Celsius");
+    Console.WriteLine("2. Miles >> Kilometers");
+    Console.WriteLine("3. Pounds >> Kilograms");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+}
+
+static void ShowDivider() {
+    Console.WriteLine("------------------------------");
+}
