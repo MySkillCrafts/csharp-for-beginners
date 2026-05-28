671108f3e0c6ce54166c8e680f68bd84be8c729d
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-05-28 20:56:31 +0200

    save progress

---
+++ Section 06 - Methods - Organizing Your Code/06 - Use Built in Methods String and Math/UnitConverter/Program.cs
@@ -0,0 +1,99 @@
+﻿
+ShowWelcome();
+
+bool running = true;
+
+while (running)
+{
+    ShowMenu();
+    Console.Write("Your choice: ");
+    string choice = Console.ReadLine();
+    ShowDivider();
+
+    if (choice == "0")
+    {
+        running = false;
+    }
+    else if (choice.ToLower() == "1")
+    {
+        Console.Write("Enter temperature in Fahrenheit: ");
+
+        string input = Console.ReadLine();
+        bool parsed = double.TryParse(input, out double fahrenheit);
+
+        if (parsed)
+        {
+            double celsius = CalculateCelsius(fahrenheit);
+            double roundedCelsius = Math.Round(celsius, 2);
+            Console.WriteLine($"{fahrenheit} °F = {roundedCelsius} °C");
+        }
+        else
+        {
+            Console.WriteLine("Invalid number. Please try again.");
+        }
+
+
+    }
+    else if (choice == "2")
+    {
+        Console.Write("Enter distance in miles: ");
+        double miles = double.Parse(Console.ReadLine());
+
+        double km = ConvertMilesToKm(miles);
+        Console.WriteLine($"{miles} miles = {km} km");
+    }
+    else if (choice == "3")
+    {
+        Console.Write("Enter weight in pounds: ");
+        double pounds = double.Parse(Console.ReadLine());
+        double kilograms = CalculateKilograms(pounds);
+        Console.WriteLine($"{pounds} lbs = {kilograms} kg");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+void ShowWelcome()
+{
+    Console.WriteLine("==============================");
+    Console.WriteLine("      UNIT CONVERTER");
+    Console.WriteLine("==============================");
+    Console.WriteLine();
+}
+
+void ShowMenu()
+{
+    Console.WriteLine("What would you like to convert?");
+    Console.WriteLine();
+    Console.WriteLine("1. Fahrenheit >> Celsius");
+    Console.WriteLine("2. Miles >> Kilometers");
+    Console.WriteLine("3. Pounds >> Kilograms");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+}
+
+static void ShowDivider()
+{
+    Console.WriteLine("------------------------------");
+}
+
+double ConvertMilesToKm(double miles)
+{
+    double result = miles * 1.60934;
+    return result;
+}
+
+double CalculateCelsius(double fahrenheit)
+{
+    double result = (fahrenheit - 32) * 5.0 / 9.0;
+    return result;
+}
+
+double CalculateKilograms(double pounds)
+{
+    double result = pounds * 0.453592;
+    return result;
+}
