e8051800c98f63917af062f259237d27c7a0e4f2
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-01 20:20:42 +0200

    save progress

---
+++ Section 08 - Objects and Classes - OOP Basics/01 - What Is OOP Objects vs Classes/StudentManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using StudentManagementSystem;
+
+Student alice = new Student();
+alice.Name = "Alice";
+alice.Grade = 95;
+
+Student bob = new Student();
+bob.Name = "Bob";
+bob.Grade = 88;
+
+Console.WriteLine($"{alice.Name}: {alice.Grade}");
+Console.WriteLine($"{bob.Name}: {bob.Grade}");
---
+++ Section 08 - Objects and Classes - OOP Basics/01 - What Is OOP Objects vs Classes/StudentManagementSystem/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem
+{
+    internal class Student
+    {
+        public string Name;
+        public int Grade;
+    }
+}
