4ad3eba7eaabe7d538b0b72781deea6c9c19d76b
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-02 23:55:49 +0200

    save progress

---
+++ Section 08 - Objects and Classes - OOP Basics/03 - Protect Data with Properties/StudentManagementSystem/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem
+{
+    internal class Student
+    {
+        private int _grade;
+
+        public string Name;
+
+        public int Grade
+        {
+            get { return _grade; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _grade = value;
+                }
+            }
+        }
+    }
+}
