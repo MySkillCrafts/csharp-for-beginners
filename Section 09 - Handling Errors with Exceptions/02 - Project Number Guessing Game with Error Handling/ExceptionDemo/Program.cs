9e34b7af1421012a03c1649a6f3909001c9df7b9
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-08 00:52:22 +0200

    save progress

---
+++ Section 09 - Handling Errors with Exceptions/02 - Project Number Guessing Game with Error Handling/ExceptionDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+try
+{
+    int number = 0;
+    int result = 10 / number;
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Cannot divide by zero: " + ex.Message);
+}
---
+++ Section 10 - Dates Time and Random Numbers/01 - Work with Dates Using DateTime/DateTimeDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTime now = DateTime.Now;
+//Console.WriteLine(now);
+Console.WriteLine($"Year: {now.Year}");
+Console.WriteLine($"Month: {now.Month}");
+Console.WriteLine($"Day: {now.Day}");
+
+Console.WriteLine($"Day of week: {now.DayOfWeek}");
+
+
+DateTime birthday = new DateTime(1990, 6, 15);
+Console.WriteLine($"Birthday: {birthday}");
+Console.WriteLine($"Born on: {birthday.DayOfWeek}");
