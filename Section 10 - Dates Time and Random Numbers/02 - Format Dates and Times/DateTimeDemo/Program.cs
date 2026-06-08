ab8c7e1ad1defb39fd3555597012e36bf15ab45a
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-08 20:12:42 +0200

    save progress

---
+++ Section 10 - Dates Time and Random Numbers/02 - Format Dates and Times/DateTimeDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿DateTime now = DateTime.Now;
+//Console.WriteLine(now);
+Console.WriteLine($"Year: {now.Year}");
+Console.WriteLine($"Month: {now.Month}");
+Console.WriteLine($"Day: {now.Day}");
+
+Console.WriteLine($"Day of week: {now.DayOfWeek}");
+
+
+DateTime birthday = new DateTime(1990, 6, 15);
+Console.WriteLine(birthday.ToString("dd/MM/yyyy"));
+Console.WriteLine(birthday.ToString("dddd, MMMM dd"));
+Console.WriteLine("-----------------------------------");
+
+Console.WriteLine(birthday.ToString("d"));
+Console.WriteLine(birthday.ToString("D"));
+Console.WriteLine(birthday.ToString("f"));
---
+++ Section 10 - Dates Time and Random Numbers/03 - Adding and Comparing Dates/DateTimeDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿DateTime today = DateTime.Now;
+
+DateTime nextWeek = today.AddDays(7);
+
+Console.WriteLine($"Today: {today.ToString("d")}");
+Console.WriteLine($"Next week: {nextWeek.ToString("d")}");
+
+DateTime nextMonth = today.AddMonths(1);
+Console.WriteLine($"Next month: {nextMonth.ToString("d")}");
+
+DateTime lastYear = today.AddYears(-1);
+Console.WriteLine($"Last year: {lastYear.ToString("d")}");
+
+if (nextWeek > today)
+{
+    Console.WriteLine("Next week is in the future.");
+}
