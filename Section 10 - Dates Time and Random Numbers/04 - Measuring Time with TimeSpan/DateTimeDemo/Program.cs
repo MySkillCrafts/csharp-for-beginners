f44273462fdea7a1e89b5d545aeaa77511694be8
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-15 14:26:39 +0200

    save progress

---
+++ Section 10 - Dates Time and Random Numbers/04 - Measuring Time with TimeSpan/DateTimeDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿DateTime start = new DateTime(DateTime.Now.Year, 1, 1);
+DateTime end = DateTime.Now;
+
+TimeSpan difference = end - start;
+Console.WriteLine($"Days since New Year: {difference.Days}");
+
+Console.WriteLine($"Total days: {difference.TotalDays}");
+
+TimeSpan twoWeeks = TimeSpan.FromDays(14);
+DateTime future = DateTime.Now + twoWeeks;
+Console.WriteLine($"Two weeks from now: {future.ToString("d")}");
---
+++ Section 10 - Dates Time and Random Numbers/05 - Generating Random Numbers/DateTimeDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = Random.Shared.Next(1, 101);
+Console.WriteLine($"Random number (1-100): {number}");
+
+
+int dice = Random.Shared.Next(1, 7);
+Console.WriteLine($"Dice roll: {dice}");
+
+string[] greetings = { "Hello!", "Hi there!", "Hey!", "Welcome!" };
+
+int index = Random.Shared.Next(0, greetings.Length);
+Console.WriteLine(greetings[index]);
