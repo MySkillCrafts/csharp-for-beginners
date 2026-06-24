c38ef8f5a793dba4faf2fa7cfc57389306bd3930
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-06-24 19:42:26 +0200

    save progress

---
+++ Section 11 - Working with Files and Text/03 - Understand File Paths/FileReadWriteDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿string folder = "MyNotes";
+string fileName = "note.txt";
+
+string fullPath = Path.Combine(folder, fileName);
+Console.WriteLine(fullPath);
+
+string path = @"C:\Users\Demo\Documents\hello.txt";
+
+Console.WriteLine(Path.GetFileName(path));
+Console.WriteLine(Path.GetExtension(path));
+Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+
+
+
+
+
+
+
+
+
---
+++ Section 11 - Working with Files and Text/04 - Create Folders and List Files/FileReadWriteDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿string folder = "MyNotes";
+
+if (!Directory.Exists(folder))
+{
+    Directory.CreateDirectory(folder);
+    Console.WriteLine("Folder created!");
+}
+else {
+    Console.WriteLine("Folder already exists.");
+}
+
+string path = Path.Combine(folder, "first-note.txt");
+File.WriteAllText(path, "My first note!");
+Console.WriteLine("Note saved.");
+
+string[] files = Directory.GetFiles(folder);
+foreach (string file in files)
+{
+    Console.WriteLine($"  {Path.GetFileName(file)}");
+}
+
+
+
+
+
+
+
+
+
+
+
