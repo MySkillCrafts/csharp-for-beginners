723b4544bcfbe3143d1a0bebe3e5724e1a37d6e5
Sergey Boiko <Sergey.Boyko@dataart.com>
2026-07-17 16:17:08 +0200

    save progress

---
+++ Section 07 - Section 07 - More CSharp Data Types/03 - Type Casting When Types Dont Match/TypesDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using TypesDemo;
+
+Season currentSeason = Season.Spring;
+
+Console.WriteLine($"Current season: {currentSeason}");
+
+if (currentSeason == Season.Spring)
+{
+    Console.WriteLine("Flowers are blooming!");
+}
+else if (currentSeason == Season.Summer)
+{
+    Console.WriteLine("Time for the beach!");
+}
