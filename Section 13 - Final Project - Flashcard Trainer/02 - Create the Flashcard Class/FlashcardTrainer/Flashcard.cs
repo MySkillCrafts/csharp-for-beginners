3166c52437eaaa4b6e7eba4e3b00da419f965d8d
Sergey Boiko <sergey.boiko4@gmail.com>
2026-07-25 19:50:51 +0200

    save progress

---
+++ Section 13 - Final Project - Flashcard Trainer/02 - Create the Flashcard Class/FlashcardTrainer/Flashcard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardTrainer
+{
+    internal class Flashcard
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Flashcard(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
+
+        public string GetPreview()
+        {
+            return $"Q: {Question}  |  A: {Answer}";
+        }
+    }
+}
---
+++ Section 13 - Final Project - Flashcard Trainer/02 - Create the Flashcard Class/FlashcardTrainer/Program.cs
@@ -0,0 +1,5 @@
+﻿using FlashcardTrainer;
+
+Flashcard sample = new Flashcard("Capital of Poland?", "Warsaw");
+
+Console.WriteLine(sample.GetPreview());
