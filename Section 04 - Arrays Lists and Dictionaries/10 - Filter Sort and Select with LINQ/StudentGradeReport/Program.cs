2549095110e1f75c6d1a39a0114c1b563e904b5d
Sergey Boiko <sergey.boiko4@gmail.com>
2026-08-17 21:27:57 +0200

    save progress

--- Section 04 - Arrays Lists and Dictionaries/10 - Filter Sort and Select with LINQ/StudentGradeReport/Program.cs
+++ Section 04 - Arrays Lists and Dictionaries/10 - Filter Sort and Select with LINQ/StudentGradeReport/Program.cs
@@ -17,32 +17,8 @@ var sortedByGrade = studentGrades.OrderByDescending(s => s.Value);
 
 var studentNames = studentGrades.Select(s => s.Key);
 
-double averageGrade = studentGrades.Average(s => s.Value);
-
-int maxGrade = studentGrades.Max(s => s.Value);
-
-int minGrade = studentGrades.Min(s => s.Value);
-
-var top3Students = studentGrades
-    .OrderByDescending(s => s.Value)
-    .Take(3)
-    .Select(s => $"{s.Key}: {s.Value}");
-
-var honorRoll = studentGrades
-    .Where(s => s.Value >= 90)
-    .OrderBy(s => s.Key)
-    .Select(s => s.Key);
-
-var bestStudent = studentGrades
-    .FirstOrDefault(s => s.Value >= 95);
-
 Section5.Lesson6.DisplayInWebBrowser(
     studentGrades,
     topStudents,
     sortedByGrade,
-    studentNames,
-    averageGrade,
-    maxGrade,
-    minGrade,
-    top3Students,
-    honorRoll);
+    studentNames);
